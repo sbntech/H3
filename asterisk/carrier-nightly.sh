0a972b897c4a197e04678e862dedf3bd449d2b13
Sbntech <sbntech@yahoo.com>
2010-10-29 15:49:51 -0700

    tweaked for monit

--- asterisk/carrier-nightly.sh
+++ asterisk/carrier-nightly.sh
@@ -3,7 +3,7 @@
 # nightly asterisk carrier refresh
 # w6 is the carrier
 
-killall asterisk
+monit stop asterisk
 sleep 1
 killall -KILL asterisk
 rm -rf /var/log/asterisk/*
@@ -12,4 +12,4 @@ sudo -u grant /bin/bash -c '(cd /home/grant/H3 ; git pull > /home/grant/nightly-
 
 /home/grant/H3/asterisk/gen-guests.pl > /home/grant/gen-guests.log
 
-asterisk
+monit start asterisk
