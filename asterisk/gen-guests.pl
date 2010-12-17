5cca861f9aa02b1b6516b8248e870728ea3519eb
Sbntech <sbntech@yahoo.com>
2010-12-17 13:24:03 -0800

    changes for chanspy

--- asterisk/gen-guests.pl
+++ asterisk/gen-guests.pl
@@ -7,9 +7,20 @@ use DialerUtils;
 
 my $dbh = DialerUtils::db_connect(); # connect to the database
 my $curCust = 0;
+my $curSpyGroup = 0;
+
+sub derive_SPYGROUP {
+	my $cust = shift;
+
+	my $curSpyGroup = 103 + ($cust % 53) * $cust;
+}
+
 
 open GSIP, '>', '/home/grant/H3/asterisk/carrier-config/guests-sip.conf'
 	or die "failed to open guest file: $!";
+
+open GEXT, '>', '/home/grant/H3/asterisk/carrier-config/guests-plan.conf'
+	or die "failed to open guest ext file: $!";
 
 my $res = $dbh->selectall_arrayref("select * 
 	from agent, project, customer
@@ -29,6 +40,29 @@ for my $row (@$res) {
 		# print customer header
 		printf "Customer: %s (Id=%d)\n", $row->{'CO_Name'}, $row->{'CO_Number'};
 		$curCust = $row->{'CO_Number'};
+		$curSpyGroup = derive_SPYGROUP($curCust);
+
+		print GEXT <<EndSpyExt
+exten => $curSpyGroup,1,ChanSpy(SIP,qwg($curSpyGroup))
+
+EndSpyExt
+;
+
+		print GSIP <<EndSpy
+[spy$curCust]
+type=friend 			
+secret=$curSpyGroup
+qualify=yes 
+host=dynamic
+insecure=port,invite
+context=starthere
+nat=yes
+canreinvite=no
+dtmfmode=auto
+call-limit=1
+
+EndSpy
+;
 	}
 
 	my $id = 'agent' . $row->{'AG_Number'};
@@ -37,6 +71,14 @@ for my $row (@$res) {
 
 	printf("  %-15s: username=%s password=%s sipAddress=sip:8$ext\@67.209.46.100:8060\n",
 		$row->{'AG_Name'}, $id, $pw);
+
+	print GEXT <<EndAgentExt
+exten => 8$ext,1,log(NOTICE,Agent extension 8$ext called)
+exten => 8$ext,n,Set(SPYGROUP=$curSpyGroup)
+exten => 8$ext,n,dial(sip/$id)
+
+EndAgentExt
+;
 
 	print GSIP <<EndAgent
 [$id]
@@ -58,6 +100,7 @@ EndAgent
     
 $dbh->disconnect;
 close GSIP;
+close GEXT;
 
 #print "\nexecuting sip reload:\n";
 #print `asterisk -x 'sip reload'`;
