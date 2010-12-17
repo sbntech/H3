45c60901815fca913bd525832a8d44e41eeff5e9
Sbntech <sbntech@yahoo.com>
2010-12-17 13:20:09 -0800

    some more test data

--- convert/database.sql
+++ convert/database.sql
@@ -326,3 +326,6 @@ INSERT INTO `reseller` VALUES (2,'ohohoh','CanadaTEST','123 Hq','Hq','HongKong',
 INSERT INTO `reseller` VALUES (79,'Winter1','Bullseye','200 Garden City Plaza','x','Garden City ','11530','NY','866-916-7695','516-706-3533','www.BullseyeBroadcast.com',1105.3264,0.016,0,'A',6,6,5,'0',1000,'Carl DAgostino','',0,'Y','M',0,50);
 INSERT INTO `customer` VALUES (1,'123','test','123 Main','yes','NewYork','87871','NY','8160009999','8908900000','yes@maybe.no',100,0.008,0.008,'A',6,6,1,'0',25,'Y','N','me','some foo','Y','T',100.0,4,1,'7027027000\r\n');
 INSERT INTO `customer` VALUES (7,'123','carlcust','123 Main','yes','NewYork','87871','NY','8160009999','8908900000','yes@maybe.no',100,0.008,0.0,'A',6,6,1,'0',25,'Y','N','me','some foo','Y','T',100.0,4,79,'7027027000\r\n');
+
+INSERT INTO `rescallerid` VALUES ('9494549999',1,'2010-12-07 12:49:47');
+INSERT INTO `custcallerid` VALUES ('9494549999',1,'2010-12-07 12:51:24');
